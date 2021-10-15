0925d0ba77b5ba6af6c188b29f2fc38c271c5f7e
farkhod <986532qw@gmail.com>
2021-10-15 11:59:48 +0300

    Initial Commit

---
+++ WeatherApplication/WeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationApp.swift
+//  WeatherApplication
+//
+//  Created by Farkhod on 15.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
